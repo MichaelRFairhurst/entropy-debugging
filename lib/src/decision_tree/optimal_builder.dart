79fcbbfee874354ab3b58ec1451e13d624fe2b3e
Mike Fairhurst <michaelrfairhurst@gmail.com>
2023-07-15 18:52:26 -0700

    Improve the optimal builder with subproblem caching.
    I should have though of this a long time ago. The optimal builder is no
    longer O(n!). All though, to be honest, I'm not sure it's current
    performance. Something like O(n^2), I'll think more on it.

--- lib/src/decision_tree/optimal_builder.dart
+++ lib/src/decision_tree/optimal_builder.dart
@@ -1,32 +1,55 @@
 import 'package:entropy_debugging/src/decision_tree/decision_tree.dart';
 import 'package:entropy_debugging/src/decision_tree/builder.dart';
 
-/// An optimal, in terms of result, [DecisionTreeBuilder]s, which takes O(n!)
-/// time to build.
+/// An optimal, in terms of result, [DecisionTreeBuilder]s, which is slow
+/// to build.
 class OptimalDecisionTreeBuilder<T> implements DecisionTreeBuilder<T> {
   @override
   DecisionTree<T> build(List<Decision<T>> decisions) {
-    if (decisions.length == 1) {
-      return decisions.single;
+    return buildSubproblem(decisions, 0, decisions.length, {});
+  }
+
+  /// Create an optimal subtree and cache it.
+  DecisionTree<T> buildSubproblem(List<Decision<T>> decisions, int start,
+      int end, Map<Range, DecisionTree<T>> cache) {
+    if (start == end - 1) {
+      return decisions[start];
     }
-    if (decisions.length == 2) {
-      return Branch<T>(decisions[0], decisions[1]);
+    if (start == end - 2) {
+      return Branch<T>(decisions[start], decisions[start + 1]);
     }
+
+    final range = Range(start, end);
+    final cached = cache[range];
+    if (cached != null) {
+      return cached;
+    }
+
     DecisionTree result;
     double resultCost;
-    final leftDecisions = [decisions.first];
-    var rightDecisions = decisions.skip(1).toList();
-    while (rightDecisions.isNotEmpty) {
-      final left = build(leftDecisions);
-      final right = build(rightDecisions);
+    for (int i = start + 1; i < end; ++i) {
+      final left = buildSubproblem(decisions, start, i, cache);
+      final right = buildSubproblem(decisions, i, end, cache);
       final contender = Branch<T>(left, right);
       if (result == null || resultCost > contender.cost) {
         result = contender;
         resultCost = result.cost;
       }
-      leftDecisions.add(rightDecisions.first);
-      rightDecisions = rightDecisions.skip(1).toList();
     }
+
+    cache[range] = result;
     return result;
   }
+}
+
+/// Class to store ranges for the sake of caching subproblem solutions.
+class Range {
+  final int start;
+  final int end;
+  Range(this.start, this.end);
+
+  int get hashCode => Object.hash(start, end);
+
+  bool operator ==(Object other) =>
+      other is Range && other.start == start && other.end == end;
 }
